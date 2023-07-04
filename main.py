69754796e4a031325376452ece29d2189e77b406
jobordeau <jovann.bordeau@gmail.com>
2023-07-04 13:12:01 +0200

    main + début de mybear avec series et iloc

---
+++ main.py
@@ -0,0 +1,17 @@
+import MyBear as mb
+
+if __name__ == '__main__':
+    # Implémentation de la classe
+    d = [35, 67, 3]
+    s = mb.Series("Ma série", d)
+
+    # Propriété iloc
+    print(s.iloc[1])
+    #print(s.iloc[1:2])
+
+    # Fonctions statistiques max, min, mean, std et count
+    print(f"max: {s.max()}")
+    print(f"min: {s.min()}")
+    print(f"mean: {s.mean()}")
+    print(f"std: {s.std()}")
+    print(f"count: {s.count()}")
---
+++ myBear.py
@@ -0,0 +1,37 @@
+import statistics
+
+
+class Series:
+    def __init__(self, nom, data):
+        self.nom = nom
+        self.data = data
+
+    @property
+    def iloc(self):
+        return Iloc(self)
+
+    def max(self):
+        return max(self.data)
+
+    def min(self):
+        return min(self.data)
+
+    def mean(self):
+        return statistics.mean(self.data)
+
+    def std(self):
+        return statistics.stdev(self.data)
+
+    def count(self):
+        return len(self.data)
+
+
+class Iloc:
+    def __init__(self, series):
+        self.series = series
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            return Series(self.series.data[index], self.series.name)
+        else:
+            return self.series.data[index]
