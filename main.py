7cc5b64f1b323129315d516386b29beca768c20d
KIANMENE TAKUEFOU CLYDE <kianmenclyder@gmail.com>
2023-07-09 21:02:06 +0200

    dataframe with iloc and statistiques functions focntional

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ if __name__ == '__main__':
     # Implémentation de la classe
     d = [35, 67, 3]
     s = mb.Series("Ma série", d)
+    # Fonctions statistiques max, min, mean, std et count pour Series
+    print(s)
 
     # Propriété iloc
     print(s.iloc[1])
     print(s.iloc[0:2])
-
-    # Fonctions statistiques max, min, mean, std et count
-    print(f"max: {s.max()}")
-    print(f"min: {s.min()}")
-    print(f"mean: {s.mean()}")
-    print(f"std: {s.std()}")
-    print(f"count: {s.count()}")
 
     # Dataframe
     # version où l'on peut charger une liste de Series en tant que DataFrame.
@@ -23,6 +18,36 @@ if __name__ == '__main__':
     df1 = mb.DataFrame(slist)
     print(df1.data)
 
-    # Une version où l'on peut directement les colonnes dans un paramètre, et les listes de valeurs dans un second paramètre.
-    df2 = mb.DataFrame([[45, -5, 9], [78, 9, 10]], ["Serie 1", "Serie 2"])
-    print(df2.data)
+    # Une version où l'on peut directement les colonnes dans un paramètre, et les listes de valeurs dans un second
+    # paramètre.
+    df2 = mb.DataFrame([[45, -5, 8], [78, 9, 10, 900]], ["Serie 1", "Serie 2"])
+    print(df2)
+
+    # iloc[n, n]
+    print(df2.iloc[0, 1])
+
+    # iloc[a:b, n]
+    print(df2.iloc[0:2, 1])
+
+    # iloc[n, a:b]
+    print(df2.iloc[2, 0:2])
+
+    # iloc[x:y, a:b]
+    print(df2.iloc[0:2, 0:2])
+
+    # Fonctions statistiques max, min, mean, std et count pour DataFrame
+
+    # max
+    print(df2.max())
+
+    # min
+    print(df2.min())
+
+    # mean
+    print(df2.mean())
+
+    # std
+    print(df2.std())
+
+    # count
+    print(df2.count())
--- myBear.py
+++ myBear.py
@@ -2,13 +2,13 @@ import statistics
 
 
 class Series:
-    def __init__(self, nom, data):
+    def __init__(self, nom: str, data: list):
         self.nom = nom
         self.data = data
 
     @property
     def iloc(self):
-        return Iloc(self)
+        return IlocSeries(self)
 
     def max(self):
         return max(self.data)
@@ -20,16 +20,22 @@ class Series:
         return statistics.mean(self.data)
 
     def std(self):
+        if len(self.data) <= 1:
+            return 0
+
         return statistics.stdev(self.data)
 
     def count(self):
         return len(self.data)
 
     def __str__(self):
-        return str(self.data)
+        toString = f"Etiquette: {self.nom} \n Donnée: {str(self.data)} \n Statistiques: \n ------------------- \n " \
+                   f"max: {self.max()} \n min: {self.min()} \n mean: {self.mean()} \n std: {self.std()} \n " \
+                   f"count: {self.count()} \n"
+        return toString
 
 
-class Iloc:
+class IlocSeries:
     def __init__(self, series):
         self.series = series
 
@@ -43,8 +49,48 @@ class Iloc:
 class DataFrame:
     def __init__(self, data=None, columns=None):
         if isinstance(data, list) and isinstance(data[0], Series):
-            self.data = {series.nom: series.data for series in data}
+            self.data = [(series.nom, series) for series in data]
         elif isinstance(data, list) and isinstance(columns, list):
-            self.data = {columns[i]: col_data for i, col_data in enumerate(data)}
+            self.data = [(columns[i], Series(columns[i], col_data)) for i, col_data in enumerate(data)]
         else:
             self.data = {}
+
+    @property
+    def iloc(self):
+        return IlocDataFrame(self)
+
+    def max(self):
+        return DataFrame([Series(serie[0], [serie[1].max()]) for serie in self.data])
+
+    def min(self):
+        return DataFrame([Series(serie[0], [serie[1].min()]) for serie in self.data])
+
+    def mean(self):
+        return DataFrame([Series(serie[0], [serie[1].mean()]) for serie in self.data])
+
+    def std(self):
+        return DataFrame([Series(serie[0], [serie[1].std()]) for serie in self.data])
+
+    def count(self):
+        return DataFrame([Series(serie[0], [serie[1].count()]) for serie in self.data])
+
+    def __str__(self):
+        toString = ""
+        for serie in self.data:
+            toString += f"{str(serie[1])} \n####################\n"
+        return toString
+
+
+class IlocDataFrame:
+    def __init__(self, dataFrame):
+        self.dataFrame = dataFrame
+
+    def __getitem__(self, index):
+        if isinstance(index[0], int) and isinstance(index[1], int):
+            return self.dataFrame.data[index[1]][1].data[index[0]]
+        elif isinstance(index[0], slice) and isinstance(index[1], int):
+            return Series(self.dataFrame.data[index[1]][0], self.dataFrame.data[index[1]][1].data[index[0]])
+        elif isinstance(index[0], int) and isinstance(index[1], slice):
+            return DataFrame([Series(serie[0], [serie[1].data[index[0]]]) for serie in self.dataFrame.data[index[1]]])
+        else:
+            return DataFrame([Series(serie[0], serie[1].data[index[0]]) for serie in self.dataFrame.data[index[1]]])
