10a5af1deb66c762af9ec645bb7953d2759fad01
KIANMENE TAKUEFOU CLYDE <kianmenclyder@gmail.com>
2023-07-11 20:31:27 +0200

    dataframe with json and csv fonctional

--- main.py
+++ main.py
@@ -6,48 +6,62 @@ if __name__ == '__main__':
     d = [35, 67, 3]
     s = mb.Series("Ma série", d)
     # Fonctions statistiques max, min, mean, std et count pour Series
-    print(s)
+    # print(s)
 
     # Propriété iloc
-    print(s.iloc[1])
-    print(s.iloc[0:2])
+    # print(s.iloc[1])
+    # print(s.iloc[0:2])
 
     # Dataframe
     # version où l'on peut charger une liste de Series en tant que DataFrame.
     slist = [mb.Series("Serie 1", [45, -5, 9]), mb.Series("Serie 2", [78, 9, 10])]
     df1 = mb.DataFrame(slist)
-    print(df1.data)
+    # print(df1)
 
-    # Une version où l'on peut directement les colonnes dans un paramètre, et les listes de valeurs dans un second
-    # paramètre.
-    df2 = mb.DataFrame([[45, -5, 8], [78, 9, 10, 900]], ["Serie 1", "Serie 2"])
-    print(df2)
+    # Une version où l'on peut directement charger les colonnes dans un paramètre, et les listes de valeurs dans un
+    # second paramètre.
+    df2 = mb.DataFrame([[45, -5, 40], [78, 9, 10, 900]], ["Serie 1", "Serie 2"])
+    # print(df2)  # pk es que lorsque je fait df1.data ca maffiche une liste de tuple comme il faut mais ca
+    # m'affiche plutot l'address des series correspondant
 
     # iloc[n, n]
-    print(df2.iloc[0, 1])
+    # print(df2.iloc[0, 1])
 
     # iloc[a:b, n]
-    print(df2.iloc[0:2, 1])
+    # print(df2.iloc[0:2, 1])
 
     # iloc[n, a:b]
-    print(df2.iloc[2, 0:2])
+    # print(df2.iloc[2, 0:2])
 
     # iloc[x:y, a:b]
-    print(df2.iloc[0:2, 0:2])
+    # print(df2.iloc[0:2, 0:2])
 
     # Fonctions statistiques max, min, mean, std et count pour DataFrame
 
     # max
-    print(df2.max())
+    # print(df2.max())
 
     # min
-    print(df2.min())
+    # print(df2.min())
 
     # mean
-    print(df2.mean())
+    # print(df2.mean())
 
     # std
-    print(df2.std())
+    # print(df2.std())
 
     # count
-    print(df2.count())
+    # print(df2.count())
+
+    # test read_csv
+    dataframe = mb.DataFrame.read_csv("test.csv")
+    print(dataframe)
+
+    # test read_json
+    # records
+    dataframe = mb.DataFrame.read_json('records.json')
+    print(dataframe)
+
+    # Columns
+    dataframe = mb.DataFrame.read_json('columns.json', "columns")
+    print(dataframe)
--- myBear.py
+++ myBear.py
@@ -1,4 +1,6 @@
 import statistics
+import csv
+import json
 
 
 class Series:
@@ -17,12 +19,13 @@ class Series:
         return min(self.data)
 
     def mean(self):
+        if len(self.data) <= 0 or isinstance(self.data[0], str):
+            return 0
         return statistics.mean(self.data)
 
     def std(self):
-        if len(self.data) <= 1:
+        if len(self.data) <= 1 or isinstance(self.data[0], str):
             return 0
-
         return statistics.stdev(self.data)
 
     def count(self):
@@ -36,31 +39,35 @@ class Series:
 
 
 class IlocSeries:
-    def __init__(self, series):
+    def __init__(self, series: Series):
         self.series = series
 
     def __getitem__(self, index):
         if isinstance(index, slice):
-            return Series(self.series.nom, self.series.data[index])
+            return Series(self.series.nom, self.series.data[
+                index])  # pk on cree une serie lorsque c'est un slice, parce que c'est dit dans l'enoncé
         else:
             return self.series.data[index]
 
 
 class DataFrame:
-    def __init__(self, data=None, columns=None):
+    def __init__(self, data=None, columns=None):  # en fonction des données entrées on crées une liste de tuple,
+        # pk avoir choisit d'utiliser une liste de tuple?
         if isinstance(data, list) and isinstance(data[0], Series):
-            self.data = [(series.nom, series) for series in data]
+            self.data = [(series.nom, series) for series in data]  # ici quand je fait series.data pk sa m'affiche
+            # uniquement l'addresse de la serie? pk ne pas mettre series.data au lieu de series?
         elif isinstance(data, list) and isinstance(columns, list):
-            self.data = [(columns[i], Series(columns[i], col_data)) for i, col_data in enumerate(data)]
+            self.data = [(columns[i], Series(columns[i], col_data)) for i, col_data in
+                         enumerate(data)]  # pk on boucle sur data et pas sur columns?
         else:
-            self.data = {}
+            self.data = []
 
     @property
     def iloc(self):
         return IlocDataFrame(self)
 
     def max(self):
-        return DataFrame([Series(serie[0], [serie[1].max()]) for serie in self.data])
+        return DataFrame([Series(serie[0], [serie[1].max()]) for serie in self.data])  # JE SUIS ICI
 
     def min(self):
         return DataFrame([Series(serie[0], [serie[1].min()]) for serie in self.data])
@@ -74,9 +81,39 @@ class DataFrame:
     def count(self):
         return DataFrame([Series(serie[0], [serie[1].count()]) for serie in self.data])
 
+    @staticmethod
+    def read_csv(path: str, delimiter: str = ","):
+        with open("test.csv", "r") as csvFile:
+            reader = csv.reader(csvFile, delimiter=delimiter)
+            columns = zip(*reader)
+            series = []
+            for column in columns:
+                series.append(Series(column[0], list(column)[1:]))
+            return DataFrame(series)
+
+    @staticmethod
+    def read_json(path: str, orient: str = "records"):
+        with open(path, "r") as json_file:
+            data = json.load(json_file)
+
+            if orient == "records":
+                series = []
+                for series_data in data:
+                    for key, value in series_data.items():
+                        series.append(Series(key, value))
+                return DataFrame(series)
+            elif orient == "columns":
+                series = []
+                for key, value in data.items():
+                    series.append(Series(key, value))
+                return DataFrame(series)
+            else:
+                return None
+
     def __str__(self):
         toString = ""
-        for serie in self.data:
+        for serie in self.data:  # data est une list de tuple, dans le tuple il n'ya que deux eleement genre le nom
+            # de la serie à l'index 0 et la serie elle meme à l'index 1
             toString += f"{str(serie[1])} \n####################\n"
         return toString
 
